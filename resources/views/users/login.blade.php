87259f46656f25e8e496f4edd590abcaffbc7fee
JakubVondra <jakubvond@seznam.cz>
2023-03-05 12:25:48 +0100

    Přidána stylizace login formuláře

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -4,23 +4,22 @@
 
 @section('content')
 
-<form method="POST" action="/users/authenticate" class="">
-    @csrf
-    <div>
-        <label class="" for="email">Email</label>
-        <input type="email" name="email" value="{{old('email')}}" />
-    </div>
-    <div>
-        <label class="" for="password">
-        Heslo
-        </label>
-        <input type="password" name="password" value="{{old('password')}}" />
-    </div>   
-    <div>
-    <button type="submit" class="">
-        Přihlaš se
-    </button>
-    </div>
-</form>
+<div class="container mx-auto text-primary">
+    <h1 class="box-heading text-center">Přihlášení</h1>
+        <form method="POST" action="/users/authenticate" class="my-3">
+            @csrf
+            <div class="form-group mt-sm-3">
+                <label for="exampleFormControlInput1">Email</label>
+                <input type="email" class="form-control mt-sm-1" id="exampleFormControlInput1" placeholder="name@example.com" value="{{old('email')}}" />
+            </div>
+            <div class="form-group mt-sm-3">
+                <label class="" for="password">Heslo</label>
+                <input class="form-control mt-sm-1" type="password" name="password" value="{{old('password')}}" />
+            </div>   
+            <div>
+            <button type="submit" class="btn btn-primary mt-sm-3">Přihlaš se</button>
+            </div>
+        </form>
+</div>
     
 @endsection
